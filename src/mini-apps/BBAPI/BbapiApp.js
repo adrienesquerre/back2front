083d694e92863752279ff5656a297f3dce91a97b
Adrien Esquerre <adrien.esquerre@gmail.com>
2025-01-03 15:22:38 +0100

    Load data from local JSON file

--- src/mini-apps/BBAPI/BbapiApp.js
+++ src/mini-apps/BBAPI/BbapiApp.js
@@ -15,7 +15,7 @@ const BBapiApp = () => {
       setIsLoading(true);
       try {
         // Fetch data from the local JSON file
-        const result = await axios('/back2front/data/characters.json'); // Adjusted path for GitHub Pages
+        const result = await axios('/data/characters.json');
         
         // Filter items based on the search query
         const filteredItems = result.data.filter((item) =>
